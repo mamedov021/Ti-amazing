673458078a699bf001c3e1dc8ee1939aa5d682b7
mamedov021 <arazmamedov021@gmail.com>
2023-11-21 17:26:56 +0400

    start

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const services = document.querySelector(".services")
+
+
+fetch("http://localhost:3000/service")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach(({id,header,img,info}) =>{
+      services.innerHTML += `
+        <div class="part">
+         <div class="image">
+          <img src=${img} alt="img" /> 
+        </div>
+          <div class="text">
+            <h2 id="part">${header}</h2>
+            <p>${info}</p>
+            <button>
+              Daha ətraflı <i class="fa-solid fa-arrow-right-long"></i>
+            </button>
+          </div>
+        </div>
+      `
+    })
+  })
+  .catch((err) => {
+    console.log(err);
+  })
