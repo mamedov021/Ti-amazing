635f1b91f80141f296a8e1293bed8d47431345ba
mamedov021 <arazmamedov021@gmail.com>
2023-11-23 18:08:59 +0400

    2K

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper2", {
   pagination: {
     el: ".swiper-pagination",
   },
 });
 
-const services = document.querySelector(".services")
-
+var swiper = new Swiper(".portfolio-swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  loop: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 4,
+    slideShadows: true,
+  },
+  loop: true,
+     
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+ 
+});
+const services = document.querySelector(".services");
 
 fetch("http://localhost:3000/service")
   .then((res) => res.json())
   .then((data) => {
-    data.forEach(({id,header,img,info}) =>{
+    data.forEach(({ header, img, info }) => {
       services.innerHTML += `
         <div class="part">
          <div class="image">
@@ -24,9 +43,37 @@ fetch("http://localhost:3000/service")
             </button>
           </div>
         </div>
-      `
-    })
+      `;
+    });
   })
   .catch((err) => {
     console.log(err);
+  });
+
+let jobs = document.querySelector(".jobs");
+let job_url = "http://localhost:3000/jobs";
+fetch(job_url)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach(({ id, name, info }) => {
+      jobs.innerHTML += `
+    <div class="job">
+     <div class="id">${id}</div>
+     <hr>
+     <div class="name">${name}</div>
+     <div class="info">${info}</div> 
+    </div>
+  `;
+      //console.log(info);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  let menu = document.querySelector('.menu');
+  let bars = document.querySelector('.bars');
+  bars.addEventListener("click", ()=> {
+    menu.style.display = "none"; 
+    console.log("ok")
   })
