bf5d8f73da946c1e5f4a6a9f616148ca48949450
mamedov021 <arazmamedov021@gmail.com>
2023-12-02 18:39:00 +0400

    lorem

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const services = document.querySelector(".services");
 
 fetch("http://localhost:3000/service")
@@ -13,9 +12,12 @@ fetch("http://localhost:3000/service")
           <div class="text">
             <h2 id="part">${header}</h2>
             <p>${info}</p>
-            <button>
-              Daha ətraflı <i class="fa-solid fa-arrow-right-long"></i>
-            </button>
+           
+             <a href="interyer.html">
+              <button> 
+             Daha ətraflı <i class="fa-solid fa-arrow-right-long"></i>
+             </button></a>
+            
           </div>
         </div>
       `;
@@ -39,44 +41,75 @@ fetch(job_url)
      <div class="info">${info}</div> 
     </div>
   `;
-       console.log(info);
+      console.log(info);
     });
-  })
-  
+  });
 
-  
-  let menu = document.querySelector('.menu');
-  let bars = document.querySelector('.bars');
-  let bars_i=document.querySelector('.bars i');
+let menu = document.querySelector(".menu");
+let bars = document.querySelector(".bars");
+let bars_i = document.querySelector(".bars i");
 
-  bars.addEventListener("click", ()=> {
-    menu.classList.toggle("active") ;
-    if(bars_i.classList.contains("fa-bars")){
-      bars_i.classList.replace("fa-bars", "fa-x")  
-    }
-    else{
-      bars_i.classList.replace("fa-x", "fa-bars")
-    }
-    
-     
-  })
- 
+bars.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  if (bars_i.classList.contains("fa-bars")) {
+    bars_i.classList.replace("fa-bars", "fa-x");
+  } else {
+    bars_i.classList.replace("fa-x", "fa-bars");
+  }
+});
 
-
-  
-  let portfolio = document.querySelector(".portfolio_container");
-  fetch("http://localhost:3000/portfolio")
-  .then((res)=>res.json())
-  .then((data)=>{
-    data.forEach(({id, img, name})=>{
-      portfolio.innerHTML +=`
+let portfolio = document.querySelector(".portfolio_container");
+fetch("http://localhost:3000/portfolio")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach(({ id, img, name }) => {
+      portfolio.innerHTML += `
       <div class="content">
       <img src="${img}" alt="">
       <h4>${name}  ${id}</h4> 
   </div>
-      `; 
-    }) 
-  }) .catch((err) => {
+      `;
+    });
+  })
+  .catch((err) => {
     console.log(err);
   });
- 
+
+let our_team = document.querySelector(".our_team");
+let members = document.querySelector(".members");
+fetch("http://localhost:3000/our_team")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach(({ img, name, prof }) => {
+      members.innerHTML += `
+    <div class="member">
+               <div><div class="img-container"> <img src="${img}" alt=""></div>
+                  <div class="info"> 
+                    <h2 class="name">${name}</h2>
+                    <h3 class="prof">${prof}</h3>
+                  </div>
+                </div>
+            </div>
+    `;
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+  let lorem_type= document.querySelector(".lorem_type");
+  fetch("http://localhost:3000/lorem")
+  .then((res)=>res.json())
+  .then((data) => {
+    data.forEach(({img})=>{
+      lorem_type.innerHTML+=`
+      <img src="${img}"> `
+
+    } )
+  })
+  .catch((err)=>{
+    console.error(err)
+  })
+
